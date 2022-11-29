ad64fa5f205ed14aef4cb4d7ed86bbac3a645a08
Chandandayal22 <108217362+Chandandayal22@users.noreply.github.com>
2022-11-29 16:04:29 +0530

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,467 @@
+document.getElementById("ctgry1").addEventListener("click", hide1);
+document.getElementById("ctgry2").addEventListener("click", hide1);
+document.getElementById("ctgry3").addEventListener("click", hide1);
+document.getElementById("ctgry4").addEventListener("click", hide1);
+//  document.getElementById("submit").addEventListener ("click" , hide2);
+
+/*--------show and hide method--------*/
+
+let x = document.getElementById("display");
+function hide1() {
+  x.style.display = "none";
+  document.getElementById("container").style.display = "block";
+}
+
+// let y = document.getElementById("container");
+//     function hide2() {
+//       y.style = "display : none";
+//       document.getElementById("result-id").style.display = "block";
+//       /*yaha a + /10 for no of question show hoga.*/
+//     }
+
+
+/*--------------questions-------------*/
+
+    Pipes = [
+      {
+        question:
+          "Two pipes A and B can fill a tank in 15 minutes and 20 minutes respectively. Both the pipes are opened together but after 4 minutes, pipe A is turned off. What is the total time required to fill the tank?",
+        a: "10 min. 20 sec.",
+        b: "11 min. 45 sec.",
+        c: "11 min. 45 sec.",
+        d: "14 min. 40 sec.",
+        correct: "c",
+      },
+      {
+        question:
+          "A tap can fill a tank in 6 hours. After half the tank is filled, three more similar taps are opened. What is the total time taken to fill the tank completely?",
+        a: "3 hrs 15 min",
+        b: "3 hrs 45 min",
+        c: "4 hrs 15 min",
+        d: "4 hrs 1 min",
+        correct: "a",
+      },
+      {
+        question:
+          "A pump can fill a tank with water in 2 hours. Because of a leak, it took 2(1/3) hours to fill the tank. The leak can drain all the water of the tank in:",
+        a: "7 hours",
+        b: "8 hours",
+        c: "12 hours",
+        d: "14 hours",
+        correct: "c",
+      },
+      {
+        question:
+          "A cistern is normally filled in 8 hours but takes two hours longer to fill because of a leak in its bottom. If the cistern is full, the leak will empty it in ?",
+        a: "20 hrs",
+        b: "28 hrs",
+        c: "36 hrs",
+        d: "40 hrs",
+        correct: "d",
+      },
+      {
+        question:
+          "Three taps A,B and C can fill a tank in 12,15 and 20 hours respectively. If A is open all the time and B ,C are open for one hour each alternatively, the tank will be full in:",
+        a: "6 hrs",
+        b: "20/3 hrs",
+        c: "7 hrs",
+        d: "15/2 hrs",
+        correct: "b",
+      },
+      {
+        question:
+          "12 buckets of water fill a tank when the capacity of each tank is 13.5 liters. How many buckets will be needed to fill the same tank,if the capacity of each bucket is 9 liters?",
+        a: "8",
+        b: "15",
+        c: "16",
+        d: "18",
+        correct: "c",
+      },
+      {
+        question:
+          "One pipe can fill a tank  three times as fast as another pipe. If together the two pipes can fill the tank in 36 min, then the slower alone will be able to fill the tank in:",
+        a: "81 min",
+        b: "108 min",
+        c: "144 min",
+        d: "192 min",
+        correct: "b",
+      },
+      {
+        question:
+          "Water flows into a tank which is 200 m long and 150 m wide, through a pipe of cross-section (0.3m x 0.2m) at 20 km/h. In what time will the water level be 12m ?",
+        a: "200 hrs",
+        b: "240 hrs",
+        c: "300 hrs",
+        d: "270 hrs",
+        correct: "b",
+      },
+      {
+        question:
+          "A cistern has a leak which would empty the cistern in 20 minutes. A tap is turned on which admits 4 liters a minute into the cistern, and it is emptied in 24 minutes. How many liters does the cistern hold ?",
+        a: "360 lit",
+        b: "480 lit",
+        c: "320 lit",
+        d: "460 lit",
+        correct: "a",
+      },
+      {
+        question:
+          "Two pipes A and B can fill a tank in 15 min and 20 min respectively. Both the pipes are opened together but after 4 min, pipe A is turned off. What is the total time required to fill the tank ?",
+        a: "15 min 20 sec.",
+        b: "16 min 40 sec.",
+        c: "13 min 10 sec.",
+        d: "14 min 40 sec.",
+        correct: "c",
+      },
+    ];
+  
+  Probability =  [
+      {
+        question:
+          "A problem is given to three students whose chances of solving it are 1/2, 1/3 and 1/4 respectively. What is the probability that the problem will be solved?",
+        a: "1/4",
+        b: "1/2",
+        c: "3/4",
+        d: "7/12",
+        correct: "b",
+      },
+      {
+        question:
+          "A bag contains 6 white and 4 black balls .2 balls are drawn at random. Find the probability that they are of same colour.",
+        a: "1/2",
+        b: "7/15",
+        c: "8/15",
+        d: "1/8",
+        correct: "a",
+      },
+      {
+        question:
+          "Tickets numbered 1 to 20 are mixed up and then a ticket is drawn at random. What is the probability that the ticket drawn has a number which is a multiple of 3 or 5?",
+        a: "1/2",
+        b: "3/5",
+        c: "9/20",
+        d: "8/15",
+        correct: "b",
+      },
+      {
+        question:
+          "Two cards are drawn at random from a pack of 52 cards.what is the probability that either both are black or both are queen?",
+        a: "52/221",
+        b: "55/190",
+        c: "55/221",
+        d: "19/221",
+        correct: "b",
+      },
+      {
+        question:
+          "A bag contains 4 white, 5 red and 6 blue balls. Three balls are drawn at random from the bag. The probability that all of them are red, is:",
+        a: " 2/91",
+        b: " 1/22",
+        c: " 3/22",
+        d: " 2/77",
+        correct: "a",
+      },
+      {
+        question:
+          "In a lottery, there are 10 prizes and 25 blanks. A lottery is drawn at random. What is the probability of getting a prize?",
+        a: "2/7",
+        b: "5/7",
+        c: "1/5",
+        d: "1/2",
+        correct: "a",
+      },
+      {
+        question:
+          "Two dice are tossed. The probability that the total score is a prime number is:",
+        a: "5/12",
+        b: "1/6",
+        c: "1/2",
+        d: "7/9",
+        correct: "a",
+      },
+      {
+        question:
+          "Three unbiased coins are tossed.What is the probability of getting at least 2 heads?",
+        a: "1/4",
+        b: " 1/2",
+        c: "3/4",
+        d: "1/3",
+        correct: "a",
+      },
+      {
+        question:
+          "In a class, there are 15 boys and 10 girls. Three students are selected at random. The probability that 1 girl and 2 boys are selected, is:",
+        a: "21/46",
+        b: "1/5",
+        c: "3/25",
+        d: "1/50",
+        correct: "a",
+      },
+      {
+        question:
+          "What is the probability of getting 53 Mondays in a leap year?",
+        a: "1/7",
+        b: "3/7",
+        c: "2/7",
+        d: "1",
+        correct: "b",
+      },
+    ];
+  
+   Age = [
+      {
+        question:
+          "A father said his son , 'I was as old as you are at present at the time of your birth.' If the father age is 38 now, the son age 5 years back was :",
+        a: "14",
+        b: "19",
+        c: "22",
+        d: "28",
+        correct: "a",
+      },
+      {
+        question:
+          "In 10 years, A will be twice as old as B was 10 years ago. If A is now 9 years older than B, the present age of B is :",
+        a: "19",
+        b: "29",
+        c: "39",
+        d: "49",
+        correct: "b",
+      },
+      {
+        question:
+          "The total age of A and B is 12 years more than the total age of B and C. C is how many years younger than A ?",
+        a: "12",
+        b: "13",
+        c: "14",
+        d: "15",
+        correct: "a",
+      },
+      {
+        question:
+          "The ratio of the present ages of P and Q is 3 : 4. Five years ago, the ratio of their ages was 5 : 7. Find their present ages.",
+        a: "18:24",
+        b: "24:32",
+        c: "15:20",
+        d: "30:40",
+        correct: "c",
+      },
+      {
+        question:
+          "'I am five times as old as you were, when I was as old as you are', said a man to his son. Find out their present ages, if the sum of their ages is 64 years ?",
+        a: "Father = 50; Son =14",
+        b: "Father = 40; Son =24",
+        c: "Father = 60; Son =14",
+        d: "Father = 48; Son =16",
+        correct: "a",
+      },
+      {
+        question:
+          "The ratio of the ages of Maala and Kala is 4 : 3. The total of their ages is 2.8 decades. The proportion of their ages after 0.8 decades will be [1 Decade = 10 years]",
+        a: "4:3",
+        b: "7:5",
+        c: "12:11",
+        d: "6:5",
+        correct: "c",
+      },
+      {
+        question:
+          "Sachin is younger than Rahul by 7 years. If the ratio of their ages is 7:9, find the age of Sachin",
+        a: "24.5",
+        b: "25.5",
+        c: "26.5",
+        d: "27.5",
+        correct: "a",
+      },
+      {
+        question:
+          "A person's present age is two-fifth of the age of his mother. After 8 years, he will be one-half of the age of his mother. How old is the mother at present ?",
+        a: "36 yrs",
+        b: "38 yrs",
+        c: "40 yrs",
+        d: "42 yrs",
+        correct: "b",
+      },
+      {
+        question:
+          "Rajeev's age after 15 years will be 5 times his age 5 years back. What is the present age of rajeev?",
+        a: "12",
+        b: "14",
+        c: "22",
+        d: "10",
+        correct: "c",
+      },
+      {
+        question:
+          "A man’s current age is 2/5 of the age of his father. After 8 years, he will be 1/2 of the age of his father. What is the age of father at now ?",
+        a: "50",
+        b: "40",
+        c: "33",
+        d: "30",
+        correct: "a",
+      },
+    ];
+  
+   Profit= [
+      {
+        question:
+          "If selling price is doubled, the profit triples. Find the profit percent ?",
+        a: "200%",
+        b: "100%",
+        c: "300%",
+        d: "400%",
+        correct: "a",
+      },
+      {
+        question:
+          "A shopkeeper cheats to the extent of 10% while buying and selling, by using false weights. His total gain is.",
+        a: "20%",
+        b: "21%",
+        c: "22%",
+        d: "3%",
+        correct: "a",
+      },
+      {
+        question:
+          "By selling 45 lemons for Rs 40, a man loses 20%. How many should he sell for Rs 24 to gain 20% in the transaction ?",
+        a: "16",
+        b: "18",
+        c: "20",
+        d: "22",
+        correct: "a",
+      },
+      {
+        question:
+          "If the cost price of 12 pens is equal to the selling price of 8 pens, the gain percent is ?",
+        a: "50%",
+        b: "20%",
+        c: "30%",
+        d: "10%",
+        correct: "a",
+      },
+      {
+        question:
+          "A milkman purchases the milk at Rs. x per litre and sells it at Rs. 2x per litre still he mixes 2 litres water with every 6 litres of pure milk. What is the profit percentage?",
+        a: "116%",
+        b: "166.66%",
+        c: "60%",
+        d: "100%",
+        correct: "a",
+      },
+      {
+        question:
+          "Tarun got 30% concession on the labelled price of an article and sold it for Rs. 8750 with 25% profit on the price he bought. What was the labelled price ?",
+        a: "10000",
+        b: "12000",
+        c: "13000",
+        d: "14000",
+        correct: "a",
+      },
+      {
+        question:
+          "Due to reduction of 25% in price of oranges a customer can purchase 4 oranges more for Rs. 16. what is original price of an orange?",
+        a: "Rs. 1",
+        b: "Rs. 1.33",
+        c: "Rs. 1.5",
+        d: "Rs. 1.6",
+        correct: "a",
+      },
+      {
+        question:
+          "A person incurs a loss of 5% be selling a watch for Rs. 1140. At what price should the watch be sold to earn 5% profit.",
+        a: "Rs.1200",
+        b: "Rs.1230",
+        c: "Rs.1260",
+        d: "Rs.1290",
+        correct: "b",
+      },
+      {
+        question:
+          "In a scheme, a pack of three soaps with MRP Rs.45 is available for Rs.42. If it still gives a profit of 5% to the shopkeeper, then the cost price of the pack is ?",
+        a: "38",
+        b: "39",
+        c: "40",
+        d: "42",
+        correct: "b",
+      },
+      {
+        question:
+          "The sale price of an article including the sales tax is Rs. 616. The rate of sales tax is 10%. If the shopkeeper has made a profit of 12%, then the cost price of the article is :",
+        a: "Rs.560",
+        b: "Rs.530",
+        c: "Rs.500",
+        d: "Rs.514",
+        correct: "b",
+      },
+    ];
+
+
+const quiz = document.getElementById("container");
+const answerElements = document.querySelectorAll(".categorytypes");
+const questionElement = document.getElementById("questionblock");
+const a_text = document.getElementById("ans1");
+const b_text = document.getElementById("ans2");
+const c_text = document.getElementById("ans3");
+const d_text = document.getElementById("ans4");
+const submitButton = document.getElementById("submit");
+
+
+
+// /*----timmer*/
+// let totalTime = 200;
+// let min = 0;
+// let sec = 0;
+// let counter = 0;
+
+
+// let timer = setInterval(function () {
+//   counter++;
+//   min = Math.floor((totalTime - counter) / 60);
+//   sec = totalTime - (min * 60) - counter;
+
+//   $(".timing span" ).text(min + ":" + sec); 
+
+// }, 1000);
+
+// /*end*/
+
+
+let currentQuiz = 0;
+let score = 0;
+const deselectAnswers = () => {
+  answerElements.forEach((answer) => (answer.checked = false));
+};
+const getSelected = () => {
+  let answer;
+  answerElements.forEach((answerElement) => {
+    if (answerElement.checked) answer = answerElement.id;
+  });
+  return answer;
+};
+/*-----pieps------*/
+const loadQuiz = () => {
+  deselectAnswers();
+  const currentQuizData = myApp[0].quizWrap[currentQuiz];
+  questionElement.innerText = currentQuizData.question;
+  a_text.innerHTML = currentQuizData.a;
+  d_text.innerHTML = currentQuizData.d;
+  b_text.innerHTML = currentQuizData.b;
+  c_text.innerHTML = currentQuizData.c;
+};
+
+loadQuiz();
+submitButton.addEventListener("click", () => {
+  const answer = getSelected();
+  if (answer) {
+    if (answer === myApp[0].quizWrap[currentQuiz].correct) score++;
+    currentQuiz++;
+    if (currentQuiz < myApp[0].quizWrap.length) loadQuiz();
+    else {
+      container.innerHTML = `
+<h2>You answered ${score}/${myApp[0].quizWrap.length} questions correctly</h2>
+<button onclick="history.go(0)">Play Again</button>
+` // location.reload() won't work in CodePen for security reasons;
+    }
+  }
+});
+
+
